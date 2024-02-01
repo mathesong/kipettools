0b44afcc949e136b652749460820a102769b2d4e
mathesong <mathesong@gmail.com>
2024-02-01 13:43:15 +0100

    Major updates to improve compatibility with latest Solena files

--- R/pipeline_outputs.R
+++ R/pipeline_outputs.R
@@ -3,6 +3,8 @@
 #' Extract TAC data from a kinfitresults.mat file
 #'
 #' @param matfile Filename of the kinfitresults file
+#' @param use_rmatio Should rmatio be used for extraction? It tends to be faster, but sometimes fails. But sometimes only rmatio works. I have no idea why...
+#' @param use_SparseM Try setting this to true if things aren't working with rmatio or without. Use without rmatio.
 #'
 #' @return List with the sizes of all rois, the subject name, PET number, and
 #'   TAC data
@@ -11,7 +13,7 @@
 #' @examples
 #' abcd_out <- kfresults_getData('abcd_1_kinfitresults.mat')
 #'
-kfresults_getData <- function(matfile) {
+kfresults_getData <- function(matfile, use_rmatio = FALSE, use_SparseM = FALSE) {
 
   # Checks
 
@@ -40,38 +42,84 @@ kfresults_getData <- function(matfile) {
 
   # Doing the deeds
 
-  kfresults <- rmatio::read.mat(matfile)
-  # kfresults <- R.matlab::readMat(matfile)
+  if(use_rmatio) {
 
-  times <- as.numeric(kfresults$data$troi[[1]]) / 60
-  durations <- c(0, as.numeric(kfresults$data$stats$times$duration[[1]]) / 60)
+    kfresults <- rmatio::read.mat(matfile)
+    # kfresults <- R.matlab::readMat(matfile)
 
-  gm_tacdata <- as.data.frame(kfresults$data$stats$greymasked$mean[[1]])
-  raw_tacdata <- as.data.frame(kfresults$data$stats$raw$mean[[1]])
+    times <- as.numeric(kfresults$data$troi[[1]]) / 60
+    durations <- c(0, as.numeric(kfresults$data$stats$times$duration[[1]]) / 60)
 
-  tacdata <- raw_tacdata
+    gm_tacdata <- as.data.frame(kfresults$data$stats$greymasked$mean[[1]])
+    raw_tacdata <- as.data.frame(kfresults$data$stats$raw$mean[[1]])
 
-  roinames <- unlist(kfresults$data$stats$roinames[[1]])
-  roinames <- gsub("#", "gm", roinames)
+    tacdata <- raw_tacdata
 
-  gmrois <- which(grepl(pattern = "gm", roinames))
+    roinames <- unlist(kfresults$data$stats$roinames[[1]])
+    roinames <- gsub("#", "gm", roinames)
 
-  tacdata[, gmrois] <- gm_tacdata[, gmrois]
+    gmrois <- which(grepl(pattern = "gm", roinames))
 
-  names(tacdata) <- roinames
+    tacdata[, gmrois] <- gm_tacdata[, gmrois]
 
-  tacdata <- rbind(0, tacdata)
+    names(tacdata) <- roinames
 
-  tacdata$times <- times
-  tacdata$durations <- durations
-  tacdata$weights <- unlist(kfresults$data$weights[[1]])
+    tacdata <- rbind(0, tacdata)
 
-  gm_roisizes <- kfresults$data$stats$greymasked$vol[[1]]
-  raw_roisizes <- kfresults$data$stats$raw$vol[[1]]
+    tacdata$times <- times
+    tacdata$durations <- durations
+    tacdata$weights <- unlist(kfresults$data$weights[[1]])
 
-  roisizes <- raw_roisizes
-  roisizes[,gmrois] <- gm_roisizes[,gmrois]
-  roisizes <- data.frame(ROI = roinames, Volume = roisizes[1,])
+    gm_roisizes <- kfresults$data$stats$greymasked$vol[[1]]
+    raw_roisizes <- kfresults$data$stats$raw$vol[[1]]
+
+    roisizes <- raw_roisizes
+    roisizes[,gmrois] <- gm_roisizes[,gmrois]
+    roisizes <- data.frame(ROI = roinames, Volume = roisizes[1,])
+
+  } else {
+
+    if(use_SparseM) {
+
+      kfresults <- R.matlab::readMat(matfile, sparseMatrixClass="SparseM")
+    } else {
+      kfresults <- R.matlab::readMat(matfile)
+    }
+
+
+    times <- as.numeric(kfresults$data[, , 1]$troi) / 60
+    durations <- c(0, as.numeric(kfresults$data[, , 1]$stats[, , 1]$times[, , 1]$duration) / 60)
+
+    gm_tacdata <- as.data.frame(kfresults$data[, , 1]$stats[, , 1]$greymasked[, , 1]$mean)
+    raw_tacdata <- as.data.frame(kfresults$data[, , 1]$stats[, , 1]$raw[, , 1]$mean)
+
+    tacdata <- raw_tacdata
+
+    roinames <- unlist(kfresults$data[, , 1]$stats[, , 1]$roinames)
+    roinames <- gsub("#", "gm", roinames)
+
+    gmrois <- which(grepl(pattern = "gm", roinames))
+
+    tacdata[, gmrois] <- gm_tacdata[, gmrois]
+
+    names(tacdata) <- roinames
+
+    tacdata <- rbind(0, tacdata)
+
+    tacdata$times <- times
+    tacdata$durations <- durations
+    tacdata$weights <- unlist(kfresults$data[, , 1]$weights[, 1])
+
+    gm_roisizes <- kfresults$data[, , 1]$stats[, , 1]$greymasked[, , 1]$vol[1, ]
+    raw_roisizes <- kfresults$data[, , 1]$stats[, , 1]$raw[, , 1]$vol[1, ]
+
+    roisizes <- raw_roisizes
+    roisizes[gmrois] <- gm_roisizes[gmrois]
+    roisizes <- data.frame(ROI = roinames, Volume = roisizes)
+
+  }
+
+
 
 
   out <- list(roisizes = roisizes, tacdata = tacdata, Subjname = Subjname, PETNo = PETNo)
@@ -87,6 +135,7 @@ kfresults_getData <- function(matfile) {
 #' Extract TAC data from a kinfitresults.mat file
 #'
 #' @param matfile Filename of the roistats file
+#' @param use_rmatio Should rmatio be used for extraction? It tends to be faster, but sometimes fails. But sometimes only rmatio works. I have no idea why...
 #'
 #' @return List with the sizes of all rois, the subject name, PET number, and
 #'   TAC data
@@ -95,7 +144,7 @@ kfresults_getData <- function(matfile) {
 #' @examples
 #' abcd_out <- roistats_getData('abcd_dynpet1_roistats.mat')
 #'
-roistats_getData <- function(matfile) {
+roistats_getData <- function(matfile, use_rmatio = FALSE) {
 
   # Checks
 
@@ -106,13 +155,20 @@ roistats_getData <- function(matfile) {
   if (n_underscores == 2) {
     details <- stringr::str_split_fixed(filename, pattern = "_", n = 2)
     Subjname <- details[, 1]
-    PETNo <- as.numeric(stringr::str_match(filename, ".*dynpet(\\d+).*")[2])
+    PETNo <- as.numeric(stringr::str_match(filename, ".*pet(\\d+).*")[2])
   }
 
   if (n_underscores == 3) {
+
     details <- stringr::str_split_fixed(filename, pattern = "_", n = 3)
-    Subjname <- paste(details[, 1], details[, 2], sep = "_")
-    PETNo <- as.numeric(stringr::str_match(filename, ".*dynpet(\\d+).*")[2])
+
+    if(stringr::str_detect(filename, "realigned_roistats")) {
+      Subjname <- details[, 1]
+      PETNo <- as.numeric(stringr::str_match(filename, ".*pet(\\d+).*")[2])
+    } else {
+      Subjname <- paste(details[, 1], details[, 2], sep = "_")
+      PETNo <- as.numeric(stringr::str_match(filename, ".*pet(\\d+).*")[2])
+    }
   }
 
   if (n_underscores != 2 & n_underscores != 3) {
@@ -124,38 +180,76 @@ roistats_getData <- function(matfile) {
 
   # Doing the deeds
 
-  roistats <- rmatio::read.mat(matfile)
-  # roistats <- R.matlab::readMat(matfile)
+  if(use_rmatio) {
 
-  times <- c(0, as.numeric(roistats$stats$times$center[[1]])) / 60
-  durations <- c(0, as.numeric(roistats$stats$times$duration[[1]]))/60
+    roistats <- rmatio::read.mat(matfile)
+    # roistats <- R.matlab::readMat(matfile)
 
-  gm_tacdata <- as.data.frame(roistats$stats$greymasked$mean[[1]])
-  raw_tacdata <- as.data.frame(roistats$stats$raw$mean[[1]])
+    times <- c(0, as.numeric(roistats$stats$times$center[[1]])) / 60
+    durations <- c(0, as.numeric(roistats$stats$times$duration[[1]]))/60
 
-  roinames <- unlist(roistats$stats$roinames[[1]])
-  roinames <- gsub("#", "gm", roinames)
-  gmrois <- which(grepl(pattern = "gm", roinames))
+    gm_tacdata <- as.data.frame(roistats$stats$greymasked$mean[[1]])
+    raw_tacdata <- as.data.frame(roistats$stats$raw$mean[[1]])
 
-  tacdata <- raw_tacdata
-  tacdata[, gmrois] <- gm_tacdata[, gmrois]
-  names(tacdata) <- roinames
-  tacdata <- rbind(0, tacdata)
+    roinames <- unlist(roistats$stats$roinames[[1]])
+    roinames <- gsub("#", "gm", roinames)
+    gmrois <- which(grepl(pattern = "gm", roinames))
 
-  tacdata$times <-times
-  tacdata$durations <- durations
-  #names(tacdata)[1] <- "Times"
+    tacdata <- raw_tacdata
+    tacdata[, gmrois] <- gm_tacdata[, gmrois]
+    names(tacdata) <- roinames
+    tacdata <- rbind(0, tacdata)
 
-  gm_roisizes <- roistats$stats$greymasked$vol[[1]]
-  raw_roisizes <- roistats$stats$raw$vol[[1]]
+    tacdata$times <-times
+    tacdata$durations <- durations
+    #names(tacdata)[1] <- "Times"
 
-  roisizes <- raw_roisizes
-  roisizes[gmrois] <- gm_roisizes[gmrois]
-  roisizes <- data.frame(ROI = roinames, Volume = roisizes[1,])
+    gm_roisizes <- roistats$stats$greymasked$vol[[1]]
+    raw_roisizes <- roistats$stats$raw$vol[[1]]
 
-  details <- stringr::str_split_fixed(basename(matfile), pattern = "_", n = 2)
-  Subjname <- details[, 1]
-  PETNo <- as.numeric(stringr::str_match(matfile, ".*dynpet(\\d+).*")[2])
+    roisizes <- raw_roisizes
+    roisizes[gmrois] <- gm_roisizes[gmrois]
+    roisizes <- data.frame(ROI = roinames, Volume = roisizes[1,])
+
+    details <- stringr::str_split_fixed(basename(matfile), pattern = "_", n = 2)
+    Subjname <- details[, 1]
+    PETNo <- as.numeric(stringr::str_match(matfile, ".*dynpet(\\d+).*")[2])
+
+  } else {
+
+    roistats <- R.matlab::readMat(matfile)
+
+    times <- c(0, as.numeric(roistats$stats[, , 1]$times[, , 1]$center)) / 60
+    durations <- c(0, as.numeric(roistats$stats[, , 1]$times[, , 1]$duration))/60
+
+    gm_tacdata <- as.data.frame(unlist(roistats$stats[, , 1]$greymasked[3][[1]]))
+    raw_tacdata <- as.data.frame(unlist(roistats$stats[, , 1]$raw[3][[1]]))
+
+    roinames <- unlist(roistats$stats[, , 1]$roinames)
+    roinames <- gsub("#", "gm", roinames)
+    gmrois <- which(grepl(pattern = "gm", roinames))
+
+    tacdata <- raw_tacdata
+    tacdata[, gmrois] <- gm_tacdata[, gmrois]
+    names(tacdata) <- roinames
+    tacdata <- rbind(0, tacdata)
+
+    tacdata$times <-times
+    tacdata$durations <- durations
+    #names(tacdata)[1] <- "Times"
+
+    gm_roisizes <- roistats$stats[, , 1]$greymasked[, , 1]$vol[1, ]
+    raw_roisizes <- roistats$stats[, , 1]$raw[, , 1]$vol[1, ]
+
+    roisizes <- raw_roisizes
+    roisizes[gmrois] <- gm_roisizes[gmrois]
+    roisizes <- data.frame(ROI = roinames, Volume = roisizes[1,])
+
+    details <- stringr::str_split_fixed(basename(matfile), pattern = "_", n = 2)
+    Subjname <- details[, 1]
+    PETNo <- as.numeric(stringr::str_match(matfile, ".*dynpet(\\d+).*")[2])
+
+  }
 
   out <- list(roisizes = roisizes, tacdata = tacdata, Subjname = Subjname, PETNo = PETNo)
 
@@ -192,9 +286,15 @@ ancmat_getData <- function(matfile) {
 
   if (n_underscores == 3) {
     details <- stringr::str_split_fixed(basename(matfile), pattern = "_", n = 4)
-    Subjname <- details[, 2]
-    PETNo <- as.numeric(details[, 3])
-  }
+
+    if(details[,3] == "arterial") {
+      Subjname <- details[, 1]
+      PETNo <- as.numeric(details[, 2])
+    } else {
+      Subjname <- details[, 2]
+      PETNo <- as.numeric(details[, 3])
+      }
+    }
 
   if (n_underscores != 2 & n_underscores != 3) {
     stop("\nThere should be either two or three underscores in the filename.
@@ -204,7 +304,6 @@ ancmat_getData <- function(matfile) {
 
 
   # Doing the deeds
-
 
   ancmat <- R.matlab::readMat(matfile)
 
@@ -384,41 +483,83 @@ ancmat_getData <- function(matfile) {
 
   ## Metabolite
 
-  if( bidsout$Metabolite$Data$units[1] == "min" ) {
+  if("units" %in% names(bidsout$Metabolite$Data)) {
+    bidsout$Metabolite$Data$Units <- bidsout$Metabolite$Data$units
+  }
+
+  if( bidsout$Metabolite$Data$Units[1] == "min" ) {
     bidsout$Blood$Continuous$Data$Units[1] = "s"
     bidsout$Blood$Continuous$Data$Values[,1] =
       bidsout$Blood$Continuous$Data$Values[,1] / 60
   }
 
-  if( bidsout$Metabolite$Data$units[1] == "min" ) {
+  if( bidsout$Metabolite$Data$Units[1] == "min" ) {
     bidsout$Blood$Continuous$Data$Units[1] = "s"
     bidsout$Blood$Continuous$Data$Values[,1] =
       bidsout$Blood$Continuous$Data$Values[,1] / 60
+  }
+
+  if( nrow(bidsout$Metabolite$Data$Values) == 0 ) {
+    bidsout$Metabolite$Data$Values <- bidsout$Plasma$Data$Values
+    bidsout$Metabolite$Data$Values[,3] <- rep(1,
+                      nrow(bidsout$Metabolite$Data$Values))
   }
 
   ## Time Warnings
 
   plasmatime_max <- max(bidsout$Plasma$Data$Values[,1])
   bloodtime_max <- max(bidsout$Blood$Discrete$Data$Values[,1])
-  bloodctime_max <- max(bidsout$Blood$Discrete$Data$Values[,1])
+  bloodctime_max <- max(bidsout$Blood$Continuous$Data$Values[,1])
   metabtime_max <- max(bidsout$Metabolite$Data$Values[,1])
 
-  if(! ( plasmatime_max > 30*60 & plasmatime_max < 150*60 )) {
+  if(! ( plasmatime_max > 30*60 & plasmatime_max < 180*60 )) {
     warning("Plasma times do not look like seconds")
   }
 
-  if(! ( bloodtime_max > 30*60 & bloodtime_max < 150*60 )) {
+  if(! ( bloodtime_max > 30*60 & bloodtime_max < 180*60 )) {
     warning("Discrete blood times do not look like seconds")
   }
 
-  if(! ( bloodctime_max > 2*60 & bloodtime_max < 12*60 )) {
+  if(! ( bloodctime_max > 1*60 & bloodctime_max < 15*60 )) {
     warning("Continuous blood times do not look like seconds")
   }
 
-  if(! ( metabtime_max > 20*60 & metabtime_max < 150*60 )) {
+  if(! ( metabtime_max > 20*60 & metabtime_max < 180*60 )) {
     warning("Metabolite times do not look like seconds")
   }
 
-  return(bidsout)
+  # if(ancmat$Blood[,,1]$Continuous[,,1]$extraInfo[,,1]$system[,1] == "ALLOGG") {
+  #   warning("dispersion constant assumed equal to 2.6 because ALLOGG system used")
+  #   dispersionconstant = 2.6
+  # } else {
+  #   warning("dispersion constant unknown. Set to 2.6")
+  #   dispersionconstant = 2.6
+  # }
+
+
+  if(Blood_Continuous$DispersionConstantUnits == "1/min") {
+    Blood_Continuous$DispersionConstantUnits == "s"
+    Blood_Continuous$DispersionConstant = 60*(1/Blood_Continuous$DispersionConstant)
+  }
+
+
+  blooddata <- kinfitr::create_blooddata_components(
+    Blood.Discrete.Values.time = bidsout$Blood$Discrete$Data$Values[,1] +
+      0.5*bidsout$Blood$Discrete$Data$Values[,2],
+    Blood.Discrete.Values.activity = bidsout$Blood$Discrete$Data$Values[,3],
+    Plasma.Values.time = bidsout$Plasma$Data$Values[,1] +
+      0.5*bidsout$Plasma$Data$Values[,2],
+    Plasma.Values.activity = bidsout$Plasma$Data$Values[,3],
+    Metabolite.Values.time = bidsout$Metabolite$Data$Values[,1] +
+      0.5*bidsout$Metabolite$Data$Values[,2],
+    Metabolite.Values.parentFraction = bidsout$Metabolite$Data$Values[,3],
+    Blood.Continuous.Values.time = bidsout$Blood$Continuous$Data$Values[,1],
+    Blood.Continuous.Values.activity = bidsout$Blood$Continuous$Data$Values[,2],
+    Blood.Continuous.DispersionConstant = Blood_Continuous$DispersionConstant,
+    Blood.Continuous.DispersionCorrected = FALSE,
+    TimeShift = 0)
+
+
+  return(blooddata)
 
 }
